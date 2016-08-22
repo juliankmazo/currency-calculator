1806042cd53523b713b36cca75e3d36098f49762
juliankmazo <julian.kmazo@gmail.com>
2016-08-22 17:33:14 -0500

    Formated total

--- app/components/currency-converter-component.js
+++ app/components/currency-converter-component.js
@@ -5,7 +5,7 @@ export default Component.extend({
   total: computed('currencyToConvert', 'totalToConvert', {
     get() {
       const total = get(this, 'currencyToConvert.rate')*get(this, 'totalToConvert');
-      return total? total.toFixed(2) : '';
+      return total? total.toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, '$1,') : '';
     }
   }),
 });
